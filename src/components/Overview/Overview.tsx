20a29139b7e170da7328972ae15cf5cf317936b8
Moaisus <64474153+Moaisus-admin@users.noreply.github.com>
2024-11-06 17:53:53 +0000

    autoplay

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import clsx from 'clsx'
+
 const logos = [
     '/images/logos/lnt.svg',
     '/images/logos/sam.svg',
@@ -13,72 +14,30 @@ const sections = [
     {
         id: 'table-of-contents',
         title: 'Maturity assessment tools',
-        link:"https://www.youtube.com/embed/gXUuPdv9_RA?autoplay=1&loop=1&playlist=gXUuPdv9_RA"
+        link: "https://www.youtube.com/embed/gXUuPdv9_RA?autoplay=1&loop=1&playlist=gXUuPdv9_RA&mute=1"
     },
-    { id: 'screencasts', title: 'Immersive Business simulations',link:"https://www.youtube.com/embed/gXUuPdv9_RA?autoplay=1&loop=1&playlist=gXUuPdv9_RA" },
-    { id: 'resources', title: 'On-demand research & consultation',link:"https://www.youtube.com/embed/gXUuPdv9_RA?autoplay=1&loop=1&playlist=gXUuPdv9_RA" },
-    { id: 'pricing', title: 'Custom learning program development',link:"https://www.youtube.com/embed/gXUuPdv9_RA?autoplay=1&loop=1&playlist=gXUuPdv9_RA" },
-
+    { id: 'screencasts', title: 'Immersive Business simulations', link: "https://www.youtube.com/embed/gXUuPdv9_RA?autoplay=1&loop=1&playlist=gXUuPdv9_RA&mute=1" },
+    { id: 'resources', title: 'On-demand research & consultation', link: "https://www.youtube.com/embed/gXUuPdv9_RA?autoplay=1&loop=1&playlist=gXUuPdv9_RA&mute=1" },
+    { id: 'pricing', title: 'Custom learning program development', link: "https://www.youtube.com/embed/gXUuPdv9_RA?autoplay=1&loop=1&playlist=gXUuPdv9_RA&mute=1" },
 ]
+
 function Overview() {
-    let [activeIndex, setActiveIndex] = useState<number >(0);
+    let [activeIndex, setActiveIndex] = useState<number>(0);
     return (
-        <div className='flex flex-col  items-center justify-center'>
-
-        <div className=' max-md:w-[100%] w-full bg-[white]    flex-col flex justify-center items-center '>
-            {/* <div className=' w-full '>
-                <div className=" sm:flex w-full sm:h-32 sm:justify-center  ">
-                    <div
-                        role="list"
-                        className="mb-[-2px]  flex gap-4   max-md:overflow-x-scroll md:justify-center w-full   bg-white text-base font-medium text-slate-900"
-                        >
-                        {sections.map((section, sectionIndex) => (
-                            <div key={section.id} className="flex relative m-auto min-w-[150px] min-h-[80px] lg:w-[236px]  xl:w-[276px] md:min-w-[150px] lg:h-[124px]  ">
-                                <a
-                                    onClick={() => setActiveIndex(sectionIndex)}
-                                    className={clsx(
-                                        'flex w-full flex-col items-center  justify-center border-b-2 before:mb-2 before:font-mono',
-                                        sectionIndex === activeIndex
-                                            ? 'border-blue-600 bg-[#004c92]  text-blue-600 before:text-blue-600'
-                                            : 'border-transparent bg-[#f2f2f2]',
-                                        )}
-                                        >
-                                    <div className={`flex flex-col gap-2 max-sm:px-2 text-sm md:text-lg text-center lg:w-[160px] ${sectionIndex === activeIndex ? "text-white " : "text-[#004c92]"} font-bold`}>
-                                        {section.title}
-                                    </div>
-                                </a>
-                                {
-                                    sectionIndex === activeIndex ? (
-                                        <div className='w-full bottom-0 absolute rounded-full h-1 bg-[white]'></div>
-                                    ) : (<div className='w-full bottom-0 absolute rounded-full h-1 bg-[#004c92]'></div>)
-                                }
-
-                            </div>
-                        ))}
+        <div className='flex flex-col items-center justify-center'>
+            <div className='max-md:w-[100%] w-full bg-[white] flex-col flex justify-center items-center'>
+                <div className='w-full flex justify-center min-w-[100%]'>
+                    <div className='w-full px-2 sm:px-5 py-[2%]'>
+                        <iframe
+                            className='min-h-[50vh] lg:min-h-[80vh] min-w-[100%] w-full'
+                            src={sections[activeIndex].link}
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        ></iframe>
                     </div>
                 </div>
-            </div> */}
-            <div className='w-full   flex justify-center min-w-[100%] '>
-
-                <div className='w-full px-2 sm:px-5  py-[2%] '>
-                    <iframe className='min-h-[50vh] lg:min-h-[80vh] min-w-[100%] w-full ' src={sections[activeIndex].link} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  ></iframe>
-
-                </div>
-
             </div>
-
         </div>
-        {/* <div className='flex max-lg:flex-wrap items-center justify-center my-10 mb-20 gap-4 md:gap-8 '>
-                {
-                    logos.map((logo, index) => (
-                        <div className=' md:min-w-[188px] min-h-[40px] md:min-h-[66px] flex justify-center items-center min-w-[188px] border border-[#FAFAFA33]  rounded-[50px]'>
-                            <img key={index} src={logo} className='max-sm:min-h-[25px] max-sm:max-h-[80px] max-sm:min-w-[200px]' alt="" />
-                        </div>
-                    ))
-                }
-
-            </div> */}
-                        </div>
     )
 }
 
