101399755feddce294274b4eb9c8c88f555d56c4
NikeVir <82455622+NikeVir@users.noreply.github.com>
2024-11-07 23:11:10 +0530

    industries feature and synopysis

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -2,13 +2,6 @@
 
 import React, { useState } from 'react';
 
-const data = [
-    "Healthcare",
-    "Pharmaceuticals",
-    "BFSI",
-    "Hospitality",
-    "Retail"
-];
 
 const FeatureDate = [
     {
@@ -28,8 +21,145 @@ const FeatureDate = [
     }
 ];
 
+
+const data: Array<keyof typeof Data> = [
+    "Healthcare",
+    "Pharmaceuticals",
+    "BFSI",
+    "Hospitality",
+    "Retail"
+]
+
+
+
+const Data = {
+    "Hospitality": {
+        title: "Can continuous learning amplify employees’ change-efficacy and contextual performance in a Post merger phase Brings You Success",
+        content: "This study investigates the relationship among continuous learning, change-efficacy and contextual performance of  employees working with a recently merged bank and further explores the mediating role of employees’ change efficacy in relationship between continuous learning and contextual performance of employees",
+        date: "May 27, 2022",
+        type: "Hospitality",
+        img: "/images/features/BFHRM03INOLL101_cover image.jpg",
+        href: "/research/hospitality",
+        more: [
+            {
+                img: "/images/features/HOHRM02INOLL101_COVER IMAGE.jpg",
+                title: "Work–life balance indicators and talent management approach: a qualitative investigation of Indian luxury hotels",
+                content: "This study investigates work–life balance (WLB) experiences of front-office employees to inductively classify a set of WLB ..."
+            },
+            {
+                img: "/images/features/ITHRM02INOLL01_Cover image.jpg",
+                title: "Continuous learning and employee performance: a moderated examination of managers’ coaching behavior in India",
+                content: "This study investigates the relationship among continuous learning, change-efficacy and contextual performance of  employees..."
+            },
+            {
+                img: "/images/features/BF001_cover image.jpg",
+                title: "  Leadership Style & Organizational Effectiveness in Indian Banking Industry",
+                content: "An extensive research involving over 100 managers in the leading banks in India was conducted to identify & delve into the leadership styles practiced by the middle management and their correlation with organization..."
+            }
+        ],
+    },
+    "Retail": {
+        title: "Continuous learning and employee performance: a moderated examination of managers’ coaching behavior in India",
+        content: "This study investigates the relationship among continuous learning, change-efficacy and contextual performance of  employees",
+        date: "May 27, 2022",
+        img: "/images/features/ITHRM02INOLL01_Cover image.jpg",
+        type: "Retail",
+        more: [
+            {
+                img: "/images/features/HOHRM02INOLL101_COVER IMAGE.jpg",
+                title: "Work–life balance indicators and talent management approach: a qualitative investigation of Indian luxury hotels",
+                content: "This study investigates work–life balance (WLB) experiences of front-office employees to inductively classify a set of WLB ..."
+            },
+            {
+                img: "/images/features/ITHRM02INOLL01_Cover image.jpg",
+                title: "Continuous learning and employee performance: a moderated examination of managers’ coaching behavior in India",
+                content: "This study investigates the relationship among continuous learning, change-efficacy and contextual performance of  employees..."
+            },
+            {
+                img: "/images/features/BF001_cover image.jpg",
+                title: "  Leadership Style & Organizational Effectiveness in Indian Banking Industry",
+                content: "An extensive research involving over 100 managers in the leading banks in India was conducted to identify & delve into the leadership styles practiced by the middle management and their correlation with organization..."
+            }
+        ],
+    },
+    "BFSI": {
+        title: "Leadership Style & Organizational Effectiveness in Indian Banking Industry",
+        content: "An extensive research involving over 100 managers in the leading banks in India was conducted to identify & delve into the leadership styles practiced by the middle management and their correlation with organization",
+        date: "May 27, 2022",
+        img: "/images/features/BF001_cover image.jpg",
+        type: "BFSI",
+        more: [
+            {
+                img: "/images/features/HOHRM02INOLL101_COVER IMAGE.jpg",
+                title: "Work–life balance indicators and talent management approach: a qualitative investigation of Indian luxury hotels",
+                content: "This study investigates work–life balance (WLB) experiences of front-office employees to inductively classify a set of WLB ..."
+            },
+            {
+                img: "/images/features/ITHRM02INOLL01_Cover image.jpg",
+                title: "Continuous learning and employee performance: a moderated examination of managers’ coaching behavior in India",
+                content: "This study investigates the relationship among continuous learning, change-efficacy and contextual performance of  employees..."
+            },
+            {
+                img: "/images/features/BF001_cover image.jpg",
+                title: "  Leadership Style & Organizational Effectiveness in Indian Banking Industry",
+                content: "An extensive research involving over 100 managers in the leading banks in India was conducted to identify & delve into the leadership styles practiced by the middle management and their correlation with organization..."
+            }
+        ],
+    },
+    "Pharmaceuticals": {
+        title: "Leadership Style & Organizational Effectiveness in Indian Banking Industry",
+        content: "An extensive research involving over 100 managers in the leading banks in India was conducted to identify & delve into the leadership styles practiced by the middle management and their correlation with organization",
+        date: "May 27, 2022",
+        img: "/images/features/BF001_cover image.jpg",
+        type: "Pharmaceuticals",
+        more: [
+            {
+                img: "/images/features/HOHRM02INOLL101_COVER IMAGE.jpg",
+                title: "Work–life balance indicators and talent management approach: a qualitative investigation of Indian luxury hotels",
+                content: "This study investigates work–life balance (WLB) experiences of front-office employees to inductively classify a set of WLB ..."
+            },
+            {
+                img: "/images/features/ITHRM02INOLL01_Cover image.jpg",
+                title: "Continuous learning and employee performance: a moderated examination of managers’ coaching behavior in India",
+                content: "This study investigates the relationship among continuous learning, change-efficacy and contextual performance of  employees..."
+            },
+            {
+                img: "/images/features/BF001_cover image.jpg",
+                title: "  Leadership Style & Organizational Effectiveness in Indian Banking Industry",
+                content: "An extensive research involving over 100 managers in the leading banks in India was conducted to identify & delve into the leadership styles practiced by the middle management and their correlation with organization..."
+            }
+        ],
+    },
+    "Healthcare": {
+        title: "Leadership Style & Organizational Effectiveness in Indian Banking Industry",
+        content: "An extensive research involving over 100 managers in the leading banks in India was conducted to identify & delve into the leadership styles practiced by the middle management and their correlation with organization",
+        date: "May 27, 2022",
+        img: "/images/features/BF001_cover image.jpg",
+        type: "Healthcare",
+        more: [
+            {
+                img: "/images/features/HOHRM02INOLL101_COVER IMAGE.jpg",
+                title: "Work–life balance indicators and talent management approach: a qualitative investigation of Indian luxury hotels",
+                content: "This study investigates work–life balance (WLB) experiences of front-office employees to inductively classify a set of WLB ..."
+            },
+            {
+                img: "/images/features/ITHRM02INOLL01_Cover image.jpg",
+                title: "Continuous learning and employee performance: a moderated examination of managers’ coaching behavior in India",
+                content: "This study investigates the relationship among continuous learning, change-efficacy and contextual performance of  employees..."
+            },
+            {
+                img: "/images/features/BF001_cover image.jpg",
+                title: "  Leadership Style & Organizational Effectiveness in Indian Banking Industry",
+                content: "An extensive research involving over 100 managers in the leading banks in India was conducted to identify & delve into the leadership styles practiced by the middle management and their correlation with organization..."
+            }
+        ],
+    },
+
+}
+
 function Features() {
-    const [isExpanded, setIsExpanded] = useState<boolean[]>(Array(FeatureDate.length).fill(false));
+    const [featureData, setFeatureData] = useState(Data.Healthcare);
+    const [isExpanded, setIsExpanded] = useState<boolean[]>(Array(featureData.more.length).fill(false));
     const [isExpanded1, setIsExpanded1] = useState(false);
     const toggleReadMore = (index: number) => {
         setIsExpanded(prevState => {
@@ -55,9 +185,12 @@ function Features() {
                 </div>
                 <div className='flex justify-center'>
                     <div className='bg-[rgba(255,255,255,0.12)] max-lg:overflow-x-scroll w-[948px] rounded-[888px] flex md:justify-center px-4 py-2 gap-[42px]'>
-                        {data.map((item, index) => (
-                            <div key={index} className='text-black text-center shadow-featurebox font-medium min-w-[150px] px-[32px] py-[10px] bg-white text-sm rounded-[30px]'>{item}</div>
-                        ))}
+                    {data.map((item, index) => (
+                            <div key={index} onClick={() => setFeatureData(
+                                Data[item as keyof typeof Data]
+                            )} className={`text-black text-center shadow-featurebox font-medium min-w-[150px] px-[32px] py-[10px] cursor-pointer  text-sm rounded-[30px] ${item == featureData.type ? "bg-[#FEBF00]" : "bg-white"}`}>{item}</div>
+                        ))
+                        }
                     </div>
                 </div>
 
@@ -65,9 +198,9 @@ function Features() {
                     <div className='flex flex-col gap-[18px] md:w-[548px]'>
                         <img src="/images/features/BFHRM03INOLL101_cover image.jpg" className='w-full h-full object-cover' alt="" />
                         <div className='flex flex-col gap-[5px] text-justify'>
-                            <p className='text-sm text-[#E8971E]'>May 27, 2022</p>
-                            <h2 className='text-2xl font-bold text-white'>Can continuous learning amplify employees’ change-efficacy and contextual performance in a Post merger phase Brings You Success</h2>
-                            <p className='text-lg text-[#BDBDBD]'>{isExpanded1 ? singleText: singleText.substring(0, 20) + "... "}
+                            <p className='text-sm text-[#E8971E]'>{featureData.date}</p>
+                            <h2 className='text-2xl font-bold text-white'>{featureData.title}</h2>
+                            <p className='text-lg text-[#BDBDBD]'>{isExpanded1 ? featureData.content: featureData.content.substring(0, 20) + "... "}
                                 <span onClick={singleToggleReadMore} className="text-[#E8971E] font-light cursor-pointer">
                                     {isExpanded1 ? "Show Less" : "Read More"}
                                 </span>
@@ -75,7 +208,7 @@ function Features() {
                         </div>
                     </div>
                     <div className='flex flex-col gap-6'>
-                        {FeatureDate.map((item, index) => (
+                        {featureData.more.map((item, index) => (
                             <div key={index} className='flex gap-4 max-w-[700px] text-justify'>
                                 <img src={item.img} className='w-[150px] h-full max-md:h-[90px] xl:w-[250px] object-cover rounded' alt="" />
                                 <div className='text-white flex flex-col gap-2'>
